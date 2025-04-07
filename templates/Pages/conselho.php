2fa9f67296ef4e3154af22d8f7883375f4ec3359
maykmorikawa <maykmorikawa@gmail.com>
2025-04-06 23:18:29 -0300

    atualização_06-03-25_01

---
+++ templates/Pages/conselho.php
@@ -0,0 +1,98 @@
+<!-- PAGE TITLE
+
+        ================================================== -->
+
+<section class="page-title-section bg-img cover-background left-overlay-dark" data-overlay-dark="6"
+    data-background="<?= WWW; ?>/site/img/pg/pg-07.jpg">
+
+    <div class="container position-unset">
+
+        <div class="page-title mx-1-6 mx-lg-2-0 mx-xl-2-6 mx-xxl-2-9">
+
+            <div class="row">
+
+                <div class="col-md-12">
+
+                    <h1>Conselho Organizacional</h1>
+
+                </div>
+
+                <div class="col-md-12">
+
+                    <ul class="ps-0">
+
+                        <li><a href="home">Home</a></li>
+
+                        <li><a href="#!">Conselho Organizacional</a></li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+
+
+<!-- SERVICE DETAILS
+
+        ================================================== -->
+
+<section>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-10 order-1 order-lg-2 mb-2-9 mb-lg-0 wow fadeIn" data-wow-delay="400ms">
+
+                <div class="row">
+                    <div class="col-12 mb-2-9">
+
+                        <h2 class="mb-3"> COMO NOS ORGANIZAMOS</h2>
+
+                        <p>O IA é formado por profissionais de diferentes áreas, qualificados e com
+                            experiência no âmbito nacional e internacional,comprovada e reconhecida,
+                            como equipe multidisciplinar de consultores e consultoras especializados (as).
+                            Essa equipe celebra as parcerias entre corporações porque, a partir delas, as
+                            ações promovem o bem-estar social, com geração de renda , acesso à
+                            educação e à alimentação.
+                        </p>
+                        <p>Além disso, constrói cenários, analisa situações, planeja, executa,
+                            acompanha e avalia projetos, de maneira sistêmica e integrada, às
+                            oportunidades estratégicas.
+                        </p>
+
+
+                    </div>
+                    <div class="col-12 mb-2-9">
+                        <h2>MURILO MONTEIRO DE SOUZA<span class="mb-0 text-primary font-weight-400 h6">- PRESIDENTE</span></h2>
+                        <h2>CARLOS ALBERTO<span class="mb-0 text-primary font-weight-400 h6">- VICE-PRESIDENTE</span></h2>
+                        <h2>MARIADACONCEIÇÃO S.N. FERREIRA<span class="mb-0 text-primary font-weight-400 h6">- DIRETORA ADMINISTRATIVA E FINANCEIRA</span></h2>
+
+                    </div>
+                    <div class="col-12 mb-2-9">
+
+                        <img src="<?= WWW; ?>/site/img/service/service-details.jpg" alt="..." class="rounded">
+
+                    </div>
+
+                   
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
