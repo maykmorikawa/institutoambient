1a8e672314b7e667f5074155f246c135c46a99d6
maykmorikawa <maykmorikawa@gmail.com>
2025-05-05 22:46:05 -0300

    atualização_05-03-25_41

--- templates/Admin/element/sidebar.php
+++ templates/Admin/element/sidebar.php
@@ -2,9 +2,9 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= $this->Url->build('/'); ?>">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon rotate-n-15">
-            <?= $this->Html->image('site/img/logos/favicon.png', ['class' => 'img', 'alt' => 'Logo']); ?>
+            <img class="img" src="<?= WWW; ?>/site/img/logos/favicon.png">
         </div>
         <div class="sidebar-brand-text mx-3">IA</div>
     </a>
@@ -14,10 +14,9 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="<?= $this->Url->build('/admin'); ?>">
+        <a class="nav-link" href="index.html">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span>
-        </a>
+            <span>Dashboard</span></a>
     </li>
 
     <!-- Divider -->
@@ -28,34 +27,34 @@
         Interface
     </div>
 
-    <!-- Nav Item - Components -->
+    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseComponents" aria-expanded="false" aria-controls="collapseComponents">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-cog"></i>
             <span>Components</span>
         </a>
-        <div id="collapseComponents" class="collapse" aria-labelledby="headingComponents" data-bs-parent="#accordionSidebar">
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/buttons'); ?>">Buttons</a>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/cards'); ?>">Cards</a>
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
             </div>
         </div>
     </li>
 
-    <!-- Nav Item - Utilities -->
+    <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUtilities" aria-expanded="false" aria-controls="collapseUtilities">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-fw fa-wrench"></i>
             <span>Utilities</span>
         </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-bs-parent="#accordionSidebar">
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/utilities-color'); ?>">Colors</a>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/utilities-border'); ?>">Borders</a>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/utilities-animation'); ?>">Animations</a>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/utilities-other'); ?>">Other</a>
+                <a class="collapse-item" href="utilities-color.html">Colors</a>
+                <a class="collapse-item" href="utilities-border.html">Borders</a>
+                <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                <a class="collapse-item" href="utilities-other.html">Other</a>
             </div>
         </div>
     </li>
@@ -68,46 +67,44 @@
         Addons
     </div>
 
-    <!-- Nav Item - Pages -->
+    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
             <i class="fas fa-fw fa-folder"></i>
             <span>Pages</span>
         </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-bs-parent="#accordionSidebar">
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Paginas</h6>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/users'); ?>">Users</a>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/posts'); ?>">Posts</a>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/tags'); ?>">Tags</a>
+                <a class="collapse-item" href="/admin/users">Users</a>
+                <a class="collapse-item" href="/admin/posts">Posts</a>
+                <a class="collapse-item" href="/admin/tags">Tags</a>
                 <div class="collapse-divider"></div>
                 <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/404'); ?>">404 Page</a>
-                <a class="collapse-item" href="<?= $this->Url->build('/admin/blank'); ?>">Blank Page</a>
+                <a class="collapse-item" href="404.html">404 Page</a>
+                <a class="collapse-item" href="blank.html">Blank Page</a>
             </div>
         </div>
     </li>
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
-        <a class="nav-link" href="<?= $this->Url->build('/admin/charts'); ?>">
+        <a class="nav-link" href="charts.html">
             <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span>
-        </a>
+            <span>Charts</span></a>
     </li>
 
     <!-- Nav Item - Tables -->
     <li class="nav-item">
-        <a class="nav-link" href="<?= $this->Url->build('/admin/tables'); ?>">
+        <a class="nav-link" href="tables.html">
             <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span>
-        </a>
+            <span>Tables</span></a>
     </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler -->
+    <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
