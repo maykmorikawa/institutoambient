07112fa08e274a8a662b4b2a088d2d989b736a87
maykmorikawa <maykmorikawa@gmail.com>
2025-05-05 23:08:06 -0300

    atualização_05-03-25_42

--- templates/Admin/layout/default.php
+++ templates/Admin/layout/default.php
@@ -33,14 +33,8 @@ $cakeDescription = 'Sistema Instituto Ambiente';
     <!-- Custom fonts for this template-->
     <link href="<?= WWW; ?>/dash/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-    <link href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote-bs4.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote-bs4.min.js"></script>
+   
+   
     <!-- Custom styles for this template-->
     <link href="<?= WWW; ?>/dash/css/sb-admin-2.min.css" rel="stylesheet">
 
@@ -106,8 +100,7 @@ $cakeDescription = 'Sistema Instituto Ambiente';
     <script src="<?= WWW; ?>/dash/js/demo/chart-area-demo.js"></script>
     <script src="<?= WWW; ?>/dash/js/demo/chart-pie-demo.js"></script>
     <script src="<?= WWW; ?>/dash/js/demo/datatables-demo.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+    
 </body>
 
 </html>
