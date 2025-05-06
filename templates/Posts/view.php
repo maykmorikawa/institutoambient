c57df208c80a4306c62dea5ff91e86376e83ce3e
Mayk Rogelio Morikawa de Souza <85082121+maykmorikawa@users.noreply.github.com>
2025-05-06 15:32:00 -0300

    atualização_06-05-25_10

--- templates/Posts/view.php
+++ templates/Posts/view.php
@@ -4,102 +4,28 @@
  * @var \App\Model\Entity\Post $post
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Post'), ['action' => 'edit', $post->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Post'), ['action' => 'delete', $post->id], ['confirm' => __('Are you sure you want to delete # {0}?', $post->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Posts'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Post'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="posts view content">
-            <h3><?= h($post->title) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Title') ?></th>
-                    <td><?= h($post->title) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Category') ?></th>
-                    <td><?= $post->hasValue('category') ? $this->Html->link($post->category->name, ['controller' => 'Categories', 'action' => 'view', $post->category->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('User') ?></th>
-                    <td><?= $post->hasValue('user') ? $this->Html->link($post->user->name, ['controller' => 'Users', 'action' => 'view', $post->user->id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Slug') ?></th>
-                    <td><?= h($post->slug) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Image') ?></th>
-                    <td><?= h($post->image) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Status') ?></th>
-                    <td><?= h($post->status) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($post->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($post->created) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Modified') ?></th>
-                    <td><?= h($post->modified) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Published') ?></th>
-                    <td><?= h($post->published) ?></td>
-                </tr>
-            </table>
-            <div class="text">
-                <strong><?= __('Content') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($post->content)); ?>
-                </blockquote>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-12">
+            <h1><?= h($post->title) ?></h1>
+            <p class="text-muted">Publicado em: <?= $post->created->format('d/m/Y H:i') ?></p>
+
+            <?php if (!empty($post->image)): ?>
+                <img src="<?= $this->Url->build('/img/uploads/' . $post->image) ?>" alt="<?= h($post->title) ?>" class="img-fluid mb-3">
+            <?php endif; ?>
+
+            <div>
+                <?= $this->Text->autoParagraph(h($post->content)); ?>
             </div>
-            <div class="text">
-                <strong><?= __('Excerpt') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($post->excerpt)); ?>
-                </blockquote>
-            </div>
-            <div class="related">
-                <h4><?= __('Related Tags') ?></h4>
-                <?php if (!empty($post->tags)) : ?>
-                <div class="table-responsive">
-                    <table>
-                        <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Name') ?></th>
-                            <th><?= __('Slug') ?></th>
-                            <th><?= __('Created') ?></th>
-                            <th><?= __('Modified') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
-                        </tr>
-                        <?php foreach ($post->tags as $tag) : ?>
-                        <tr>
-                            <td><?= h($tag->id) ?></td>
-                            <td><?= h($tag->name) ?></td>
-                            <td><?= h($tag->slug) ?></td>
-                            <td><?= h($tag->created) ?></td>
-                            <td><?= h($tag->modified) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'Tags', 'action' => 'view', $tag->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'Tags', 'action' => 'edit', $tag->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Tags', 'action' => 'delete', $tag->id], ['confirm' => __('Are you sure you want to delete # {0}?', $tag->id)]) ?>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
-                </div>
+
+            <div class="mt-4">
+                <strong>Tags:</strong>
+                <?php if (!empty($post->tags)): ?>
+                    <?php foreach ($post->tags as $tag): ?>
+                        <span class="badge bg-secondary"><?= h($tag->name) ?></span>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <span class="text-muted">Nenhuma tag relacionada.</span>
                 <?php endif; ?>
             </div>
         </div>
