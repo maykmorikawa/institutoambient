a6c2907a994c98479600e0f1cafa27dbab4523af
Mayk Rogelio Morikawa de Souza <85082121+maykmorikawa@users.noreply.github.com>
2025-05-12 18:12:23 -0300

    Atualizar o AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -26,6 +26,7 @@ class AppController extends Controller
             'display',
             'home',
             'noticia',
+            'tags',
             'view',
             'manutencao' // libera /pages/display/*, inclusive /pages/home
         ]);
