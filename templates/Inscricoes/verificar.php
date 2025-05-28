097280605180eec4a411a93d9387d1b0675953b2
Mayk Morikawa <maykmorikawa@gmail.com>
2025-05-28 15:54:42 -0300

    atualização_28-05-25_11

--- templates/Inscricoes/verificar.php
+++ templates/Inscricoes/verificar.php
@@ -1,13 +1,16 @@
-<section class="page-title-section bg-img cover-background left-overlay-dark" data-overlay-dark="6" data-background="<?= WWW; ?>/site/img/banner/page-title.jpg">
+<section class="page-title-section bg-img cover-background left-overlay-dark" data-overlay-dark="6"
+    data-background="<?= WWW; ?>/site/img/banner/page-title.jpg">
     <div class="container position-unset">
         <div class="page-title mx-1-6 mx-lg-2-0 mx-xl-2-6 mx-xxl-2-9">
             <div class="row">
                 <div class="col-md-12">
-                    <h1>Verificar Inscrição</h1> </div>
+                    <h1>Verificar Inscrição</h1>
+                </div>
                 <div class="col-md-12">
                     <ul class="ps-0">
                         <li><a href="/">Home</a></li>
-                        <li><a href="#!">Verificar Inscrição</a></li> </ul>
+                        <li><a href="#!">Verificar Inscrição</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -23,13 +26,17 @@
                         <h1>Inscrição em: <?= h($atividade->nome) ?></h1>
 
                         <?= $this->Form->create() ?>
-                         <?= $this->Flash->render() ?>
-                            <?= $this->Form->control('cpf', ['label' => 'Seu CPF', 'required' => true , 'class' => 'form-control']) ?>
-                            <?= $this->Form->control('data_nascimento', ['type' => 'date', 'label' => 'Data de Nascimento','required' => true ,'class' => 'form-control']) ?>
-                            <?= $this->Form->button('Verificar Cadastro', ['class' => 'butn-style3 mt-3']) ?>
+                        <?php foreach ($message as $msg): ?>
+                            <div class="alert-danger custom-alert">
+                                <?= h($msg) ?>
+                            </div>
+                        <?php endforeach; ?>
+                        <?= $this->Form->control('cpf', ['label' => 'Seu CPF', 'required' => true, 'class' => 'form-control']) ?>
+                        <?= $this->Form->control('data_nascimento', ['type' => 'date', 'label' => 'Data de Nascimento', 'required' => true, 'class' => 'form-control']) ?>
+                        <?= $this->Form->button('Verificar Cadastro', ['class' => 'butn-style3 mt-3']) ?>
                         <?= $this->Form->end() ?>
                     </div>
-                    </div>
+                </div>
             </div>
         </div>
     </div>
