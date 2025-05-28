832eba6efb3ebe20463e967a3567dcaaffaa3975
Mayk Morikawa <maykmorikawa@gmail.com>
2025-05-28 16:10:50 -0300

    atualização_28-05-25_12

--- templates/Inscricoes/verificar.php
+++ templates/Inscricoes/verificar.php
@@ -1,16 +1,45 @@
-<section class="page-title-section bg-img cover-background left-overlay-dark" data-overlay-dark="6"
-    data-background="<?= WWW; ?>/site/img/banner/page-title.jpg">
+<style>
+    /* Flash messages */
+.message {
+    padding: .5rem 1rem;
+    background: var(--color-message-info-bg);
+    color: var(--color-message-info-text);
+    border-color: var(--color-message-info-border);
+    border-width: 1px;
+    border-style: solid;
+    border-radius: 4px;
+    margin-bottom: 1rem;
+    cursor: pointer;
+}
+.message.hidden {
+    display: none;
+}
+.message.success {
+    background: var(--color-message-success-bg);
+    color: var(--color-message-success-text);
+    border-color: var(--color-message-success-border);
+}
+.message.warning {
+    background: var(--color-message-warning-bg);
+    color: var(--color-message-warning-text);
+    border-color: var(--color-message-warning-border);
+}
+.message.error {
+    background: var(--color-message-error-bg);
+    color: var(--color-message-error-text);
+    border-color: var(--color-message-error-border);
+}
+</style>
+<section class="page-title-section bg-img cover-background left-overlay-dark" data-overlay-dark="6" data-background="<?= WWW; ?>/site/img/banner/page-title.jpg">
     <div class="container position-unset">
         <div class="page-title mx-1-6 mx-lg-2-0 mx-xl-2-6 mx-xxl-2-9">
             <div class="row">
                 <div class="col-md-12">
-                    <h1>Verificar Inscrição</h1>
-                </div>
+                    <h1>Verificar Inscrição</h1> </div>
                 <div class="col-md-12">
                     <ul class="ps-0">
                         <li><a href="/">Home</a></li>
-                        <li><a href="#!">Verificar Inscrição</a></li>
-                    </ul>
+                        <li><a href="#!">Verificar Inscrição</a></li> </ul>
                 </div>
             </div>
         </div>
@@ -26,17 +55,13 @@
                         <h1>Inscrição em: <?= h($atividade->nome) ?></h1>
 
                         <?= $this->Form->create() ?>
-                        <?php foreach ($message as $msg): ?>
-                            <div class="alert-danger custom-alert">
-                                <?= h($msg) ?>
-                            </div>
-                        <?php endforeach; ?>
-                        <?= $this->Form->control('cpf', ['label' => 'Seu CPF', 'required' => true, 'class' => 'form-control']) ?>
-                        <?= $this->Form->control('data_nascimento', ['type' => 'date', 'label' => 'Data de Nascimento', 'required' => true, 'class' => 'form-control']) ?>
-                        <?= $this->Form->button('Verificar Cadastro', ['class' => 'butn-style3 mt-3']) ?>
+                         <?= $this->Flash->render() ?>
+                            <?= $this->Form->control('cpf', ['label' => 'Seu CPF', 'required' => true , 'class' => 'form-control']) ?>
+                            <?= $this->Form->control('data_nascimento', ['type' => 'date', 'label' => 'Data de Nascimento','required' => true ,'class' => 'form-control']) ?>
+                            <?= $this->Form->button('Verificar Cadastro', ['class' => 'butn-style3 mt-3']) ?>
                         <?= $this->Form->end() ?>
                     </div>
-                </div>
+                    </div>
             </div>
         </div>
     </div>
