b2136539a7b679e825342320b3ea46a2e29acf50
Mayk Morikawa <maykmorikawa@gmail.com>
2025-05-28 12:36:05 -0300

    atualização_28-05-25_02

--- templates/Admin/Users/login.php
+++ templates/Admin/Users/login.php
@@ -12,9 +12,9 @@
                         <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                         <div class="col-lg-6">
                             <div class="p-5">
-                                
-                                
-                                
+
+
+
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Bem-vindo de volta!</h1>
                                 </div>
@@ -38,8 +38,14 @@
                                 <?= $this->Flash->render() ?>
                                 <div class="form-group">
                                     <div class="custom-control custom-checkbox small">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                                        <label class="custom-control-label" for="customCheck">Lembrar-me</label>
+                                        <?= $this->Form->control('remember_me', [
+                                            'type' => 'checkbox',
+                                            'label' => 'Lembrar-me',
+                                            'class' => 'custom-control-input',
+                                            'id' => 'customCheck',
+                                            'labelOptions' => ['class' => 'custom-control-label'],
+                                            'hiddenField' => false // impede envio automático de "0"
+                                        ]) ?>
                                     </div>
                                 </div>
                                 <?= $this->Form->button(__('Entrar'), ['class' => 'btn btn-primary btn-user btn-block']) ?>
