05d10e3645fabdd3aae3aef7850726f71b45f7a9
Mayk Morikawa <maykmorikawa@gmail.com>
2025-06-06 16:36:00 -0300

    atualização_06-06-25_09

--- templates/Alunos/add.php
+++ templates/Alunos/add.php
@@ -328,17 +328,36 @@
                                 <h5 class="mb-0 text-white">Informação Obrigatorias</h5>
                             </div>
                             <div class="card-body">
-                                <?= $this->Form->control('autorizo_imagem', [
-                                    'class' => 'form-control',
-                                    'type' => 'checkbox',
-                                    'label' => 'AUTORIZO: o uso de minha imagem em todo e qualquer material entre fotos, documentos, meios de comunicação, mídias sociais (Facebook, Instagran, WhatsApp, etc) para ser utilizada em campanhas promocionais e institucional do INSTITUTO AMBIENT – IA, desde que não haja desvirtuamento da sua finalidade. A presente autorização é concedida a título gratuito, abrangendo o uso da imagem acima mencionada em todo território nacional e no exterior, em todas as suas modalidades e, em destaque, das seguintes formas: (I) out-door; (II) busdoor; folhetos em geral (encartes, mala direta, catálogo, etc.); (III) folder de apresentação; (IV) anúncios em revistas e jornais em geral; (V) home page; (VI) cartazes; (VII) back-light; (VIII) mídia eletrônica (painéis, vídeo-tapes, televisão, cinema, programa para rádio, entre outros).'
-                                ]) ?>
 
-                                <?= $this->Form->control('compromisso_participacao', [
-                                    'class' => 'form-control',
-                                    'type' => 'checkbox',
-                                    'label' => 'COMPROMETO-ME a participar das ações e eventos socias organizados gratuitamente pelo Instituto Ambient, como uma estratégia roativa de contribuição à sociedade.'
-                                ]) ?>
+                                <div class="form-check mb-4">
+                                    <?= $this->Form->checkbox('autorizo_imagem', ['class' => 'form-check-input', 'id' => 'autorizoImagem']) ?>
+                                    <label class="form-check-label" for="autorizoImagem">
+                                        <strong>AUTORIZO:</strong> o uso de minha imagem em todo e qualquer material entre fotos,
+                                        documentos, meios de comunicação, mídias sociais (Facebook, Instagran, WhatsApp,
+                                        etc) para ser utilizada em campanhas promocionais e institucional do INSTITUTO
+                                        AMBIENT - IA, desde que não haja desvirtuamento da sua finalidade. A presente
+                                        autorização é concedida a título gratuito, abrangendo o uso da imagem acima
+                                        mencionada em todo território nacional e no exterior, em todas as suas
+                                        modalidades e, em destaque, das seguintes formas: (I) out-door; (II) busdoor;
+                                        folhetos em geral (encartes, mala direta, catálogo, etc.); (III) folder de
+                                        apresentação; (IV) anúncios em revistas e jornais em geral; (V) home page; (VI)
+                                        cartazes; (VII) back-light; (VIII) mídia eletrônica (painéis, vídeo-tapes,
+                                        televisão, cinema, programa para rádio, entre outros).
+                                    </label>
+                                </div>
+
+                                <div class="form-check mb-4">
+                                    <?= $this->Form->checkbox('compromisso_participacao', [
+                                        'class' => 'form-check-input',
+                                        'id' => 'compromissoParticipacao'
+                                    ]) ?>
+                                    <label class="form-check-label text-justify" for="compromissoParticipacao"
+                                        style="font-size: 0.95rem;">
+                                        <strong>COMPROMETO-ME</strong> a participar das ações e eventos sociais
+                                        organizados gratuitamente pelo Instituto Ambient, como uma estratégia proativa
+                                        de contribuição à sociedade.
+                                    </label>
+                                </div>
                             </div>
                         </div>
 
