5e72cb311ad1d6a63a0f7c28acc1f97accbbcf1f
Mayk Morikawa <maykmorikawa@gmail.com>
2025-07-31 16:59:58 -0300

    atualização_31-07-25_03

--- templates/Pages/transparencia.php
+++ templates/Pages/transparencia.php
@@ -71,7 +71,7 @@
                     </div>
                     <div class="col-md-4 position-relative elements-block mb-6 mb-md-0">
                         <div class="inner-title">
-                            <h2 class="mb-0">Demonstrações Contábeis</h2>
+                            <h2 class="mb-0">Demonstrações</h2>
                         </div>
                         <ul class="list-style4">
                             <a href="<?= WWW; ?>/uploads/pdfs/DRE_e_Balanco_IA_2022_2023.pdf" target="_blank">
