b52f2d0e8d91940ef8134b144a50e027610560a9
Mayk Rogelio Morikawa de Souza <85082121+maykmorikawa@users.noreply.github.com>
2025-11-03 10:59:07 -0300

    atualização-03-11-25-01

--- templates/Pages/atuacao.php
+++ templates/Pages/atuacao.php
@@ -167,3 +167,52 @@
         </div>
     </div>
 </section>
+
+<section class="service-block pt-0">
+    <div class="container position-relative z-index-1">
+        <div class="row align-items-center mb-1-9 mb-lg-6 wow fadeIn" data-wow-delay="100ms">
+            <div class="col-lg-5 mb-3 mb-lg-0">
+                <span class="d-block mb-2 text-secondary text-uppercase fw-bold">Instituto Ambient</span>
+                <h2 class="mb-0">Eixos Estratégicos de Atuação</h2>
+            </div>
+            <div class="col-lg-7">
+                <p class="mb-0 border-lg-start border-width-4 border-secondary-color py-lg-4 ps-lg-6">O trabalho do Instituto Ambient está estruturado em eixos de atuação que se efetivam em projetos sociais e socioambientais, guiando a organização para alcançar seus objetivos estratégicos.</p>
+            </div>
+        </div>
+        <div class="row mt-n1-9">
+            <div class="col-md-6 col-lg-4 col-xl-3 mt-1-9 wow fadeIn" data-wow-delay="200ms">
+                <a href="#!" class="card card-style2 border-0 h-100">
+                    <div class="card-body px-1-9 py-5 py-sm-6 text-center">
+                        <i class="ti-heart display-10 mb-4 d-block"></i> <h3 class="h5 mb-3">EIXO 01: Potencialização da capacidade protetiva das famílias em situação de vulnerabilidade social</h3>
+                        <p class="mb-0">**Objetivo:** Contribuir no desenvolvimento da capacidade protetiva das famílias em situação de vulnerabilidade social, com ações de caráter preventivo, protetivo e proativo. [cite: 462, 463]</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-4 col-xl-3 mt-1-9 wow fadeIn" data-wow-delay="400ms">
+                <a href="#!" class="card card-style2 border-0 h-100">
+                    <div class="card-body px-1-9 py-5 py-sm-6 text-center">
+                        <i class="ti-money display-10 mb-4 d-block"></i> <h3 class="h5 mb-3">EIXO 02: Proporcionando Geração de emprego e renda</h3>
+                        <p class="mb-0">**Objetivo:** Promover ações de qualificação e inserção no mundo do trabalho, com fomento a ações e serviços com uso de tecnologias inovadoras e negócios sociais. [cite: 575, 589]</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-4 col-xl-3 mt-1-9 wow fadeIn" data-wow-delay="600ms">
+                <a href="#!" class="card card-style2 border-0 h-100">
+                    <div class="card-body px-1-9 py-5 py-sm-6 text-center">
+                        <i class="ti-sharethis display-10 mb-4 d-block"></i> <h3 class="h5 mb-3">EIXO 03: Redes e assessoramento institucional</h3>
+                        <p class="mb-0">**Objetivo:** Prestar serviços de assessoramento voltados ao fortalecimento das organizações da sociedade civil, movimentos sociais e lideranças comunitárias. [cite: 610]</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 col-lg-4 col-xl-3 mt-1-9 wow fadeIn" data-wow-delay="800ms">
+                <a href="#!" class="card card-style2 border-0 h-100">
+                    <div class="card-body px-1-9 py-5 py-sm-6 text-center">
+                        <i class="ti-plus display-10 mb-4 d-block"></i> <h3 class="h5 mb-3">EIXO 04: Saúde e bem estar</h3>
+                        <p class="mb-0">**Objetivo:** Proporcionar noções de cuidados de forma integrativa, promovendo a qualidade de vida. [cite: 617]</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <img src="img/content/dots1.png" class="position-absolute bottom-n40 right d-none d-lg-block wow fadeIn ani-left-right" data-wow-delay="200ms" alt="...">
+</section>
