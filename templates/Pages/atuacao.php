2118b80b1557a4ead4e1a9788a2a7b98627f31dd
mayk morikawa <maykmorikawa@gmail.com>
2025-11-03 20:36:57 -0300

    atualização-03-11-2025-03

--- templates/Pages/atuacao.php
+++ templates/Pages/atuacao.php
@@ -167,6 +167,98 @@
             </div>
         </div>
     </div>
+</section><section class="pt-0">
+    <div class="container">
+        <div class="text-center mb-2-9 mb-lg-6 wow fadeIn" data-wow-delay="100ms">
+            <span class="text-secondary mb-2 d-block fw-bold text-uppercase">Instituto Ambient</span>
+            <h2 class="mb-0 h1">Pressupostos Estratégicos</h2>
+            <p class="mt-4 mx-auto w-md-75">Para o direcionamento do presente planejamento estratégico, estamos focados em pressupostos que consideramos fundamentais e que deverão fazer a diferença na vida dos participantes.</p>
+        </div>
+        
+        <div class="row">
+
+            <div class="row align-items-center mb-5 pb-4 wow fadeIn" data-wow-delay="200ms">
+                <div class="col-lg-6 order-lg-1 order-2">
+                    <div class="pe-0 pe-lg-3 pe-xl-4">
+                        <h4 class="mb-3 text-secondary">1. Capacidade Protetiva das Famílias</h4>
+                        <p>As famílias desenvolvem uma **vital habilidade protetiva**, agregando processos dinâmicos de vínculos sociais e territoriais que qualificam suas relações familiares.</p>
+                        <p>Esta capacidade protetiva conjuga o protagonismo na efetivação de processos, condições e ambientes seguros e protetivos para todos.</p>
+                        <ul class="list-style1 mb-4">
+                            <li>Foco no protagonismo e autonomia familiar.</li>
+                            <li>Promoção de ambientes seguros e protetivos.</li>
+                        </ul>
+                        <a href="#!" class="butn-style3 medium">Saiba Mais</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-lg-2 order-1 mb-4 mb-lg-0">
+                    <div class="box-shadow-large">
+                        <img src="img/about/familia-protetiva.jpg" alt="Imagem ilustrativa de uma família unida e protegida." class="img-fluid">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center mb-5 pb-4 wow fadeIn" data-wow-delay="400ms">
+                <div class="col-lg-6 mb-4 mb-lg-0">
+                    <div class="box-shadow-large">
+                        <img src="img/about/pedagogia-critica.jpg" alt="Imagem ilustrativa de aprendizado e desenvolvimento crítico." class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="ps-0 ps-lg-3 ps-xl-4">
+                        <h4 class="mb-3 text-secondary">2. Pedagogia Sociocultural Crítica</h4>
+                        <p>Adotamos abordagens em conformidade com uma **pedagogia sociocultural crítica**, que percebe o indivíduo como **sujeito**, nunca como objeto. Este sujeito é constituinte de faculdade produtiva.</p>
+                        <p>Ele tem potencial de engajamento nos processos e capacidades de questionar, investigar, planejar, corrigir e sistematizar a vida, levando em conta seu contexto social.</p>
+                        <ul class="list-style1 mb-4">
+                            <li>O foco é o sujeito com faculdade produtiva.</li>
+                            <li>Estímulo à dinâmica criativa e ao questionamento.</li>
+                        </ul>
+                        <a href="#!" class="butn-style3 medium">Nossa Abordagem</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center mb-5 pb-4 wow fadeIn" data-wow-delay="600ms">
+                <div class="col-lg-6 order-lg-1 order-2">
+                    <div class="pe-0 pe-lg-3 pe-xl-4">
+                        <h4 class="mb-3 text-secondary">3. Processos Intersetoriais</h4>
+                        <p>Os **processos intersetoriais** são uma premissa fundamental, correspondendo à necessidade intrínseca de colaboração e comunicação eficaz entre diferentes setores.</p>
+                        <p>Essa colaboração é vital para alcançar objetivos complexos e promover resultados mais abrangentes e sustentáveis em nossas áreas de atuação (Saúde, Educação, Assistência Social e Meio Ambiente).</p>
+                        <ul class="list-style1 mb-4">
+                            <li>Comunicação e colaboração eficazes.</li>
+                            <li>Promoção de resultados abrangentes e sustentáveis.</li>
+                        </ul>
+                        <a href="#!" class="butn-style3 medium">Nossas Áreas</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-lg-2 order-1 mb-4 mb-lg-0">
+                    <div class="box-shadow-large">
+                        <img src="img/about/intersetorial.jpg" alt="Imagem ilustrativa de diferentes setores trabalhando em conjunto." class="img-fluid">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center wow fadeIn" data-wow-delay="800ms">
+                <div class="col-lg-6 mb-4 mb-lg-0">
+                    <div class="box-shadow-large">
+                        <img src="img/about/tecnologia-inovacao.jpg" alt="Imagem ilustrativa de tecnologia e inovação." class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="ps-0 ps-lg-3 ps-xl-4">
+                        <h4 class="mb-3 text-secondary">4. Tecnologia e Inovação</h4>
+                        <p>Implementamos **processos e procedimentos tecnológicos** que potencializam a transformação de ideias inovadoras em realidades, por meio de linguagens artísticas e meios de comunicação.</p>
+                        <p>Isso permite a criação de produtos, serviços, metodologias e experiências que promovem a nossa missão.</p>
+                        <ul class="list-style1 mb-4">
+                            <li>Uso de linguagens artísticas e meios de comunicação.</li>
+                            <li>Criação de produtos, serviços, metodologias e experiências inovadoras.</li>
+                        </ul>
+                        <a href="#!" class="butn-style3 medium">Nossos Projetos</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </section>
 <!-- SERVICE
 ================================================== -->
