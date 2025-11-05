a3a93b22302ff8ddb8b8ec16e1910a37f4d3e0b5
mayk morikawa <maykmorikawa@gmail.com>
2025-11-04 23:53:07 -0300

    atualização-04-11-2025-33

--- config/routes.php
+++ config/routes.php
@@ -92,7 +92,8 @@ return function (RouteBuilder $routes): void {
       $builder->connect('/manutencao', ['controller' => 'Pages', 'action' => 'manutencao']);
 
       $builder->connect('/lado-a-lado', ['controller' => 'Pages', 'action' => 'display', 'ladoalado']);
-      
+      $builder->connect('/contato', ['controller' => 'Pages', 'action' => 'display', 'contato']);
+
       $builder->connect('/contato', ['controller' => 'Contacts', 'action' => 'index']);
       $builder->connect('/contato/enviar', ['controller' => 'Contatos', 'action' => 'enviar']);
 
