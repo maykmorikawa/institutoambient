bc1aca9fb4085c3ef7833851cf61c118d7afbc1b
mayk morikawa <maykmorikawa@gmail.com>
2025-11-04 22:27:01 -0300

    atualização-04-11-2025-25

--- templates/Pages/contato.php
+++ templates/Pages/contato.php
@@ -1,4 +1,4 @@
-<section class="page-title-section bg-img cover-background left-overlay-dark" data-overlay-dark="6" data-background="img/banner/page-title.jpg">
+<section class="page-title-section bg-img cover-background left-overlay-dark" data-overlay-dark="6" data-background="<?= WWW; ?>/site/img/banner/page-title.webp">
     <div class="container position-unset">
         <div class="page-title mx-1-6 mx-lg-2-0 mx-xl-2-6 mx-xxl-2-9">
             <div class="row">
