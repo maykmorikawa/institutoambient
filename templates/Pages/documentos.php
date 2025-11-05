15020d6624dda873b02600c0a6bef3f69b9b3a91
Mayk Rogelio Morikawa de Souza <85082121+maykmorikawa@users.noreply.github.com>
2025-11-05 11:21:39 -0300

    atualização-05-11-25-03

--- templates/Pages/documentos.php
+++ templates/Pages/documentos.php
@@ -48,64 +48,33 @@
                             <h2 class="mb-0">Ducumentos do IA</h2>
                         </div>
                         <ul class="list-style4">
-                            <a href="<?= WWW; ?>/uploads/pdfs/Relatorio_de_Atividades_IA_2023.pdf" target="_blank">
-                                <li>Relatório de Atividades IA - 2023</li>
+                            <a href="<?= WWW; ?>/uploads/pdfs/ESTATUTO_IA.pdf" target="_blank">
+                                <li>Estatuto do Instituto Ambiente</li>
                             </a>
                         </ul>
                         <ul class="list-style4" style="margin-top: 10px">
-                            <a href="<?= WWW; ?>/uploads/pdfs/Relatorio_de_Atividades_2024_com_Balanco.pdf" target="_blank">
-                                <li>Relatório de Atividades IA - 2024</li>
+                            <a href="<?= WWW; ?>/uploads/pdfs/PLANO_DE_COMUNICAO_E_MARKETING_INSTITUTO_AMBIENTE_OFICIAL.pdf" target="_blank">
+                                <li>Plano de comunicação e marketing</li>
+                            </a>
+                        </ul>
+                        <ul class="list-style4" style="margin-top: 10px">
+                            <a href="<?= WWW; ?>/uploads/pdfs/COMPLIANCE_IA_CODIGO_DE_CONDURA_E_ETICA.pdf" target="_blank">
+                                <li>Compliance IA Código de Conduta e Ética</li>
+                            </a>
+                        </ul>
+                        <ul class="list-style4" style="margin-top: 10px">
+                            <a href="<?= WWW; ?>/uploads/pdfs/PLANO_DE_CAPTACAO_DE_REUCURSOS.pdf" target="_blank">
+                                <li>Plano de Capitação de Recursos</li>
+                            </a>
+                        </ul>
+                        <ul class="list-style4" style="margin-top: 10px">
+                            <a href="<?= WWW; ?>/uploads/pdfs/PLANEJAMENTO_INSTITUCIONAL_2025_2028.pdf" target="_blank">
+                                <li>Planejamento Institucional 2025 à 2028</li>
                             </a>
                         </ul>
                         
                     </div>
-                    <div class="col-md-4 position-relative elements-block mb-6 mb-md-0">
-                        <div class="inner-title">
-                            <h2 class="mb-0">DRE e Balanço</h2>
-                        </div>
-                        <ul class="list-style4">
-                            <a href="<?= WWW; ?>/uploads/pdfs/DRE_e_Balanco_IA_2022_2023.pdf" target="_blank">
-                                <li>DRE e Balanço IA 2022 - 2023</li>
-                            </a>
-                        </ul>
-                        <ul class="list-style4"style="margin-top: 10px">
-                            <a href="<?= WWW; ?>/uploads/pdfs/DRE_e_Balanco_IA_2022_2023.pdf" target="_blank">
-                                <li>DRE e Balanço IA 2023 - 2024</li>
-                            </a>
-                        </ul>
-                    </div>
-                    <div class="col-md-4 position-relative elements-block mb-6 mb-md-0">
-                        <div class="inner-title">
-                            <h2 class="mb-0">Demonstrações</h2>
-                        </div>
-                        <ul class="list-style4">
-                            <a href="<?= WWW; ?>/uploads/pdfs/Relatorio_do_Auditor_Independente_Inst_Ambient_2024_assinado.pdf" target="_blank">
-                                <li>Demonstrações Contábeis - 2024</li>
-                            </a>
-                        </ul>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="row">
-                    <div class="col-md-4 position-relative elements-block mb-6 mb-md-0">
-                        <div class="inner-title">
-                            <h2 class="mb-0">Relatório</h2>
-                        </div>
-                        <ul class="list-style4">
-                            <a href="<?= WWW; ?>/uploads/pdfs/Ata_IA_Prestatacao_de_Contas_2025.pdf" target="_blank">
-                                <li>Ata - IA - Prestatação de Contas 2025</li>
-                            </a>
-                        </ul>                        
-                    </div>
+                   
                 </div>
             </div>
         </div>
