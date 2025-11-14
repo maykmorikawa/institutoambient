90e34a88ae3f48445debf9ea5a1e8988264aa52c
Mayk Rogelio Morikawa de Souza <85082121+maykmorikawa@users.noreply.github.com>
2025-11-14 10:11:57 -0300

    atualização-14-11-25_06

--- templates/Pages/eixos1.php
+++ templates/Pages/eixos1.php
@@ -43,48 +43,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-12"> <div class="mb-6 mb-lg-8 position-relative elements-block">
                     <div class="inner-title">
-                        <h2 class="mb-0">QUADRO SÍNTESE EIXOS, OBJETIVOS, AÇÕES ESTRATÉGICAS E PROJETOS INSTITUCIONAIS</h2>
-                    </div>
-                    
-                    <div class="table-responsive"> <table class="table table-bordered"> <thead>
-                                <tr>
-                                    <th scope="col" style="width: 30%;">EIXOS E OBJETIVOS</th>
-                                    <th scope="col" style="width: 35%;">AÇÕES ESTRATÉGICAS</th>
-                                    <th scope="col" style="width: 35%;">PROJETOS INSTITUCIONAIS</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="align-top">
-                                        **EIXO 01:** Potencialização da capacidade protetiva das famílias em situação de vulnerabilidade social
-                                        <hr>
-                                        **OBJETIVO ESTRATÉGICO:** Contribuir no desenvolvimento da capacidade protetiva das famílias em situação de vulnerabilidade social, com ações de caráter preventivo, protetivo e proativo na direção do fortalecimento de vínculos familiares, comunitários e pela melhoria da qualidade de suas vidas.
-                                    </td>
-                                    
-                                    <td class="align-top">
-                                        <ul>
-                                            <li>Proporcionar trabalho social com famílias em situação de vulnerabilidade, a partir de uma proteção social proativa, com orientação individual, coletiva, articulação e encaminhamento a um conjunto de políticas, programas, projetos e serviços sociais que objetivem uma ampliação da rede de proteção e apoio dessas famílias, constituindo um tecido social consistente que resguarda os seus direitos fundamentais.</li>
-                                            <li>Oportunizar processos e espaços socioeducativos de convivência, formação e participação cidadã, por ciclos de vida e processos intergeracionais, para o fortalecimento de vínculos e prevenção as diversas formas de violências, considerando intervenções protagonizadas por experiências lúdicas, culturais e esportivas, com vista a garantia da expressão, interação, aprendizagem, sociabilidade e proteção social dos sujeitos envolvidos.</li>
-                                            <li>Propiciar trabalho social com pessoas com deficiências e idosas, ativando uma proteção proativa, espaços e processos de convivências na perspectiva de prevenção de agravos que possam provocar o rompimento de vínculos familiares e comunitários.</li>
-                                        </ul>
-                                    </td>
-                                    
-                                    <td class="align-top">
-                                        <ul>
-                                            <li>Convivência e Fortalecimento de Vínculos familiares</li>
-                                            <li>Serviço de Convivência e Fortalecimento de Vínculos (SCFV) - Parceria: FUNPAPA</li>
-                                            <li>VOZES DAS ÁGUAS: Arte e Educação nos territórios ribeirinhos de Belém/Pará.</li>
-                                            <li>FLORES DO OÁSIS: Artesanato sustentável e geração de renda.</li>
-                                            <li>OÁSIS: Envelhecimento ativo e inclusão de mulheres 50+.</li>
-                                            <li>ACESSIBILIDADE: processos inclusivos às pessoas com deficiência</li>
-                                            <li>CRESCER E TRANSFORMAR: Construindo um Futuro Digital - Parceria: Equatorial</li>
-                                            <li>PROJETO AURORA: Apoio a Mulheres Grávidas Vulneráveis nos Municípios de Belém e Jacundá (Parceria: Ministério das Mulheres)</li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                </tbody>
-                        </table>
-                    </div>
+                        <h2 class="mb-0"> PROJETOS INSTITUCIONAIS</h2>
                     </div>
             </div>
         </div>
