44374d87f9e7489ae5646bda650db9b6ca163a5b
mayk morikawa <maykmorikawa@gmail.com>
2026-04-02 12:16:50 -0300

    atualização-02-04-26-01

--- templates/Admin/Posts/edit.php
+++ templates/Admin/Posts/edit.php
@@ -76,12 +76,12 @@
                                             <?php endif; ?>
                                         </div>
                                         <div class="card-body p-2 bg-light border-top">
-                                            <div class="custom-control custom-radio mb-2">
+                                            <div class="custom-control custom-radio mb-2 p-2 bg-white rounded border shadow-xs px-4">
                                                 <input type="radio" class="custom-control-input" name="featured_image_id" 
                                                        value="<?= $img->id ?>" id="featured_<?= $img->id ?>" <?= $img->is_featured ? 'checked' : '' ?>>
                                                 <label class="custom-control-label small font-weight-bold" for="featured_<?= $img->id ?>">Destaque</label>
                                             </div>
-                                            <div class="custom-control custom-checkbox bg-white rounded px-3 py-1 border">
+                                            <div class="custom-control custom-checkbox p-2 bg-white rounded border shadow-xs px-4">
                                                 <input type="checkbox" class="custom-control-input" name="delete_images[<?= $img->id ?>]" 
                                                        value="1" id="delete_<?= $img->id ?>">
                                                 <label class="custom-control-label small text-danger font-weight-bold" for="delete_<?= $img->id ?>">Remover</label>
