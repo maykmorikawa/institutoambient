e990fac7b4cbbb11dec20ec49c4bf486b8a8367d
mayk morikawa <maykmorikawa@gmail.com>
2026-05-12 21:11:55 -0300

    atualização-12-05-26-01

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -45,7 +45,7 @@
                     <div class="col-lg-6">
                         <div class="banner-image-wrapper">
                             <div class="banner-image-frame">
-                                <img src="<?= WWW; ?>/site/img/banner/slide-03.webp" alt="Apoio à Família">
+                                <img src="<?= WWW; ?>/site/img/banner/slide-001.webp" alt="Apoio à Família">
                                 <div class="banner-logos-bar">
                                     <img src="<?= WWW; ?>/site/img/icons/icon-10.png" alt="Parceiro">
                                     <img src="<?= WWW; ?>/site/img/icons/icon-11.png" alt="Parceiro">
@@ -77,7 +77,39 @@
                     <div class="col-lg-6">
                         <div class="banner-image-wrapper">
                             <div class="banner-image-frame">
-                                <img src="<?= WWW; ?>/site/img/banner/slide-04.webp" alt="Meio Ambiente">
+                                <img src="<?= WWW; ?>/site/img/banner/slide-002.webp" alt="Meio Ambiente">
+                                <div class="banner-logos-bar">
+                                    <img src="<?= WWW; ?>/site/img/icons/icon-4.png" alt="Parceiro">
+                                    <img src="<?= WWW; ?>/site/img/icons/icon-5.png" alt="Parceiro">
+                                    <img src="<?= WWW; ?>/site/img/icons/icon-6.png" alt="Parceiro">
+                                    <img src="<?= WWW; ?>/site/img/avatar/avatar-02.png" alt="Logo">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Slide 3 -->
+        <div class="item pt-6 pb-14 py-md-16 py-lg-20 py-xxl-24">
+            <div class="container pt-6 pt-md-0">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 mb-1-9 mb-lg-0 py-5">
+                        <span class="text-primary display-21 display-sm-19 display-md-17 display-lg-14 mb-3 d-block text-uppercase fw-bold">Sustentabilidade</span>
+                        <h1 class="text-white display-16 display-md-9 display-lg-6 display-xl-3 mb-1-6 font-weight-700">
+                            Meio <span class="text-primary">Ambiente</span>
+                        </h1>
+                        <p class="text-white mb-2-3 opacity8 display-md-28 w-lg-90">
+                            Ações para melhorar a vida dos cidadãos através da preservação e consciência ecológica.
+                        </p>
+                        <a href="#!" class="btn-banner-custom">
+                            Saiba Mais <i class="fas fa-arrow-right ms-2"></i>
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="banner-image-wrapper">
+                            <div class="banner-image-frame">
+                                <img src="<?= WWW; ?>/site/img/banner/slide-003.webp" alt="Meio Ambiente">
                                 <div class="banner-logos-bar">
                                     <img src="<?= WWW; ?>/site/img/icons/icon-4.png" alt="Parceiro">
                                     <img src="<?= WWW; ?>/site/img/icons/icon-5.png" alt="Parceiro">
